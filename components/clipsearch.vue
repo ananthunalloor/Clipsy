f9676992431bbf594dfd9b1b3f0cb28142e1d123
ananthunalloor <ananthunalloor@gmail.com>
2021-08-26 19:08:37 +0530

    changing forms

--- components/clipsearch.vue
+++ components/clipsearch.vue
@@ -2,9 +2,11 @@
   <div class="h-full">
     <div class="absolute clip-card w-full top-1/4" v-if="card">
       <div class="absolute left-3/4 m-8">
-    <button v-on:click="card = !card"><i class="text-4xl text-gray-100 hover:text-red-500 fas fa-times"></i></button>
-  </div>
-    <Card/>
+        <button v-on:click="card = !card">
+          <i class="text-4xl text-gray-100 hover:text-red-500 fas fa-times"></i>
+        </button>
+      </div>
+      <Card />
     </div>
     <div class="w-full mt-32 mb-40">
       <div class="text-center p-4">
@@ -16,18 +18,14 @@
         >
       </div>
       <div class="pt-8 text-center px-2">
-        <input
-          class="p-4 lg:w-6/12 sm:w-11/12 md:w-9/12 rounded-lg bg-gray-300.. text-blue-400 just-nuni"
-          type="url"
-          name=""
-          placeholder="https://clips.twitch.com"
-        />
-        <button v-on:click="card = !card"
-          class="px-8 py-4 m-2 bg-gray-200 hover:bg-purple-600 rounded-lg"
-        >
-          <span class="text-gray-900 hover:text-gray-100 just-nuni"
-            >GET CLIP</span
-          >
+        <span class="flex items-center justify-center text-red-500 text-xs just-nuni">
+          Invalid username field !</span>
+        <input v-model="cliplink"
+          class="p-4 lg:w-6/12 sm:w-11/12 md:w-9/12 rounded-lg bg-gray-300 text-blue-400 just-nuni"
+          type="url" name="searchclip" pattern="https://.*" placeholder="https://clips.twitch.com"/>
+        <button v-on:click="getCliplink()"
+          class="px-8 py-4 m-2 bg-gray-200 hover:bg-purple-600 rounded-lg">
+          <span class="text-gray-900 hover:text-gray-100 just-nuni">GET CLIP</span>
         </button>
       </div>
     </div>
@@ -35,15 +33,23 @@
 </template>
 <script>
 export default {
-data(){
-  return{
-    card: false,
-  }
-}
+  data() {
+    return {
+      card: false,
+      cliplink: '',
+      errStyle: ' border-2 border-red-500',
+    }
+  },
+  methods: {
+    getCliplink() {
+      console.log(this.cliplink)
+      this.card = !this.card
+    },
+  },
 }
 </script>
 <style scoped>
-.clip-card{
-    background-color: rgba(7, 24, 39, .8);
+.clip-card {
+  background-color: rgba(7, 24, 39, 0.8);
 }
 </style>
