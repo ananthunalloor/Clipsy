707603e4925fa5b0af52f30b6ee5306c9a6d685c
ananthunalloor <ananthunalloor@gmail.com>
2021-08-30 12:45:49 +0530

    about twitch added

--- components/about.vue
+++ components/about.vue
@@ -6,14 +6,22 @@
       </div>
       <div class="md:p-8 sm:p-2">
         <span class="nuni text-xl">
-          <h4>
-            Follow these simple steps.
-          </h4>
+          <h4>Follow these simple steps.</h4>
         </span>
-        <ol class="list-decimal list-inside text-gray-900 reg-nuni py-2 text-lg">
-          <li>Copy the URL from your favorite clips that you want to download.</li>
-          <li>Paste the copied URL in the search box and click the “Get Clip” button.</li>
-          <li>In the pop-up, make sure that it is the clip that you want to download and click the “Download” button. </li>
+        <ol
+          class="list-decimal list-inside text-gray-900 reg-nuni py-2 text-lg"
+        >
+          <li>
+            Copy the URL from your favorite clips that you want to download.
+          </li>
+          <li>
+            Paste the copied URL in the search box and click the “Get Clip”
+            button.
+          </li>
+          <li>
+            In the pop-up, make sure that it is the clip that you want to
+            download and click the “Download” button.
+          </li>
           <li>There you go, your clip will be downloaded to your device.</li>
         </ol>
       </div>
@@ -23,12 +31,64 @@
       <div class="md:p-8 sm:p-4">
         <span class="text-gray-900 reg-nuni text-lg">
           <p>
-            Clippy is an easy way to download clips from your favorite Streamer / Stream on Twitch.
-Built for you to save your favorite clip or most memorable moments on Twitch. 
-<span class="font-bold italic">
-This website has no affiliation to <a class="hover:text-purple-500" target="_blank" href="https://twitch.tv">twitch.tv</a>.</span> Don't forget to support favorite streamers through Donations and Subs.
+            Clippy is an easy way to download clips from your favorite Streamer
+            / Stream on Twitch. Built for you to save your favorite clip or most
+            memorable moments on Twitch.
+            <span class="font-bold italic">
+              This website has no affiliation to
+              <a
+                class="hover:text-purple-500"
+                target="_blank"
+                href="https://twitch.tv"
+                >twitch.tv</a
+              >.</span
+            >
+            Don't forget to support favorite streamers through Donations and
+            Subs.
           </p>
         </span>
+      </div>
+      <div class="px-2 mx-2 py-1 border-l-4 border-purple-500">
+        <span class="text-gray-900 text-2xl reg-nuni"
+          ><h4>About Twitch</h4></span
+        >
+      </div>
+      <div class="md:p-8 sm:p-4">
+        <span class="text-gray-900 reg-nuni text-lg">
+          <p>
+            Twitch is an American video live-streaming service that focuses on
+            video game live-streaming, including broadcasts of esports
+            competitions. In addition, it offers music broadcasts, creative
+            content, and more recently, “in real life” streams. It is operated
+            by Twitch Interactive, a subsidiary of Amazon.com, It was introduced
+            in June 2011 as a spin-off of the general-interest streaming
+            platform Justin.tv. Content on the site can be viewed either live or
+            via video on demand.
+          </p>
+          <p>
+            Twitch remained the leading live-streaming video service for video
+            games in the US, and had an advantage over YouTube Gaming. As of
+            February 2020, it had 3 million broadcasters monthly and 15 million
+            daily active users, with 1.4 million average concurrent users.
+          </p>
+        </span>
+        <div class="flex justify-between just-nuni">
+          <div class="p-3">
+            <span class="text-sm bg-gray-200 p-2 italic"
+              >Source : Wikipedia</span
+            >
+          </div>
+          <div class="text-blue-600">
+            <span class="text-base hover:underline"
+              ><a
+                href="https://en.wikipedia.org/wiki/Twitch_(service)"
+                target="_blank"
+              >
+                twitch wiki</a
+              ></span
+            >
+          </div>
+        </div>
       </div>
     </div>
   </div>
